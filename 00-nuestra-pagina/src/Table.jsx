c168cdfebd8e3616d449c9cbf921255a5a8a9b40
Mariano <Marianofogel17@gmail.com>
2024-07-31 17:50:25 -0300

    Actualizamos.

--- 00-nuestra-pagina/src/Table.jsx
+++ 00-nuestra-pagina/src/Table.jsx
@@ -1,8 +1,3 @@
-import { useState } from "react"
-import React, { Component } from "react";
-import BigCalendar from "react-big-calendar";
-import moment from "moment";
-
 export function Tablon(){ 
     return(
         <h2>Creamos la Tablarda Lepes</h2>
